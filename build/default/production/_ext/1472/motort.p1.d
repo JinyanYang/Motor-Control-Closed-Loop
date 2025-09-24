ce720f88341e5f6f958a2e3213f228e1e16f69dc
JinyanYang <146944980+JinyanYang@users.noreply.github.com>
2025-09-24 21:15:38 +0800

    Delete build/default/production directory

--- build/default/production/_ext/1472/motort.p1.d
+++
@@ -1,3 +0,0 @@
-build/default/production/_ext/1472/motort.p1:  \
-../motort.c  \
-../motort.h 
--- build/default/production/_ext/293650525/Buttons_Debounce.p1.d
+++
@@ -1,3 +0,0 @@
-build/default/production/_ext/293650525/Buttons_Debounce.p1:  \
-../Buton_Debounce_and_LibraryFolders/Button_Debounce_Library/Buttons_Debounce.c  \
-../Buton_Debounce_and_LibraryFolders/Button_Debounce_Library/Buttons_Debounce.h 
